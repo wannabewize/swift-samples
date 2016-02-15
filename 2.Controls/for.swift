9f43e073f6b325c0d5c6674e0d798df02d3803ec
wannabewize <wannabewize@gmail.com>
2016-02-15 23:47:01 +0900

    For 예제

---
+++ 2.Controls/for.swift
@@ -0,0 +1,21 @@
+// For Loop
+for var i = 0 ; i < 10 ; i++ {
+   // print(i);   
+}
+
+// With Array
+var array = [1, 2, 3, 4, 5]
+for var i = 0 ; i < array.count ; i++ {
+   // print(i);
+}
+
+// With Array2
+for i in array {
+   // print(i)
+}
+
+// With Dictionary
+var dic1 = [ 0:"zero", 1:"one", 2:"two", 3:"three", 4:"Four", 5:"Five" ]
+for (num, eng) in dic1 {   
+   // print(num, eng)
+}
