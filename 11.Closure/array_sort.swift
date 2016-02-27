c5c25406779a6ac58475f6a40e736e89588f60ae
Lee Jaehoon <wannabewize@gmail.com>
2016-02-27 14:39:54 +0900

    클로저 단축 표현 추가

--- 11.Closure/array_sort.swift
+++ 11.Closure/array_sort.swift
@@ -17,20 +17,24 @@ sorted2
 
 // 타입 선언 생략
 let sorted3 = array.sort( { a, b -> Bool in return a < b } )
-sorted3
+print(sorted3)
 
 // 1줄인 경우 return 생략
 let sorted4 = array.sort( { a, b -> Bool in a < b } )
-sorted4
+print(sorted4)
+
+// 반환 타입 선언 생략
+let sorted5 = array.sort({ a, b in a < b })
+print(sorted5)
 
 // 파라미터 선언 생략
-let sorted5 = array.sort({ $0 < $1 })
-sorted5
+let sorted6 = array.sort({ $0 < $1 })
+print(sorted6)
 
 // Trailing
-let sorted6 = array.sort{ $0 < $1 }
-sorted6
+let sorted7 = array.sort{ $0 < $1 }
+print(sorted7)
 
 // Operator Function
-let sorted7 = array.sort( < )
-sorted7
+let sorted8 = array.sort( < )
+print(sorted8)
