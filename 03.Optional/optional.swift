d05e731f6b693f5f58995a3561c23d2298a7c0e7
Lee Jaehoon <wannabewize@gmail.com>
2016-02-28 14:49:49 +0900

    옵셔널 예제. 예제 파일 이름 변경,

--- 03.Optional/optional.swift
+++ 03.Optional/optional.swift
@@ -1,11 +1,26 @@
+// Error
+//var i : Int = nil
+// Optional Type
+var optionalVar : Int? = nil
+
+
+// nil 원소를 다룰 수 있는 배열
+let array : [String?] = ["A", nil, "C"]
+let element = array[0] // element는 옵셔널 타입
+print(element)
+
+
 var optionalStr : String? = "ABC"
+
+// OptionalType 직접 사용은 불가!
 //optionalStr.lowercaseString
 
-
+// if를 이용한 옵셔널 바인딩
 if let str = optionalStr {
    print(str.lowercaseString)
 }
 
+// guard를 이용한 옵셔널 바인딩
 func myFunc(arg : String?) {
    guard let str = arg else {
       print("파라미터가 nil")
@@ -19,14 +34,7 @@ func myFunc(arg : String?) {
 myFunc(nil)
 
 
+// ?? 연산자
 var userSelectedColor : String?
-// nil 이면 Red를 사용
-var colorName = userSelectedColor ?? "Red"
-
-colorName // "Red"
-
-
-// nil 원소를 다룰 수 있는 배열
-let array : [String?] = ["A", nil, "C"]
-let element = array[0] // element는 옵셔널 타입
-print(element)
+var colorName = userSelectedColor ?? "Red"  // nil 이면 Red를 사용
+print(colorName) // "Red"
