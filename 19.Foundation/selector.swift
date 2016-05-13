0c6d39633ab8796335c9507bd11dd1e1e5e2c731
wannabewize <wannabewize+macbookpro@gmail.com>
2016-05-13 19:27:03 +0900

    셀렉터 : swift 2.2에 변경 내용 예제

--- 19.Foundation/selector.swift
+++ 19.Foundation/selector.swift
@@ -1,74 +1,146 @@
 import Foundation
 
-
 class MyClass : NSObject {
    func greeting() {
       print("Hello")
    }
-     
+   
    func justDoIt() {
-      // Instance 생략하기. 혹은 self 사용
-      let innerSel = #selector(greeting)
-      print(innerSel)
-   }    
+      let sel = #selector(greeting)
+      print(sel)
+   }
 }
 
 let obj = MyClass()
-let sel = #selector(obj.greeting)
-print(sel)
 
+// Selector - outside
+let sel1 = #selector(obj.greeting)
+print(sel1)
+print(obj.respondsToSelector(sel1))
+
+// Selector - declared in Inside
 obj.justDoIt()
 
-/*
-func greeting(person : String) {
+
+class MyClass2 {
+   @objc func greeting() {
+      print("Hello")
+   }
 }
 
-Selector("greeting:")
+let obj2 = MyClass2()
+let sel2 = #selector(obj2.greeting)
 
-
-func greeting(person : String, with emotion : String) {
+class MyClass3 : NSObject {
+   func greeting(person : String) {
+      print("How are you? \(person)")
+   }
+   
+   func greeting(who person : String) {
+      print("How are you? \(person)")
+   }
+   
+   func greeting(person : String, emotion : String) {
+   }
+   
+   func greeting(person : String, with emotion : String) {
+   }
+   
+   // 첫 번째 파라미터에 외부 파라미터 이름 설정
+   func greeting(who person : String, emotion : String) {
+   }
+   
+   // 두 번째 파라미터에 외부 파라미터 이름 생략
+   func greeting(person : String, _ emotion : String) {
+   }
 }
 
-func greeting(person : String, emotion : String) {
+let obj3 = MyClass3()
+let sel3 = #selector(obj3.greeting(_:))
+let sel4 = #selector(obj3.greeting(who:))
+
+
+let sel5 = #selector(obj3.greeting(_:emotion:))
+let sel6 = #selector(obj3.greeting(_:with:))
+let sel7 = #selector(obj3.greeting(who:emotion:))
+let sel8 = #selector(obj3.greeting(_:_:))
+
+print(sel5)
+print(sel6)
+print(sel7)
+print(sel8)
+
+
+class MyClass4 : NSObject {
+   func greeting() {
+      
+   }
+   
+   func greeting(who : String) {
+   }
+   
+   func justDoIt() {
+      let sel1 = #selector(greeting as Void -> Void)
+      print(sel1)
+      
+      let sel2 = #selector(greeting(_:))
+      print(sel2)
+   }
 }
 
-Selector("greeting:with:")
-// 내부 파라미터 이름을 외부 파라미터 이름으로 사용
-Selector("greeting:emotion:")
+let obj4 = MyClass4()
+obj4.justDoIt()
 
-class MyClass : NSObject {
+
+class MyClass5 : NSObject {
+   static func goodbye() {}
+   func justDoIt() {
+      let sel = #selector(MyClass5.goodbye)
+      print(sel)
+   }
+   
+   static func goodnight() {}
+   static func goodnight(who : String) {}
+}
+
+let sel9 = #selector(MyClass5.goodbye)
+print(sel9)
+
+let sel10 = #selector(MyClass5.goodbye as Void -> Void)
+let sel11 = #selector(MyClass5.goodnight(_:))
+
+
+class MyClass6 : NSObject {
+   override init() {}
+   init(name : String) {}
+   
+   func justDoIt() {
+      let sel = #selector(MyClass6.init(name:))
+   }
+}
+
+let sel12 = #selector(MyClass6.init as Void -> MyClass6)
+let sel13 = #selector(MyClass6.init(name:))
+
+
+class YesClass : NSObject {
    func sayYes() {
       print("YES!")
    }
 }
 
-var obj = MyClass()
+let yes = YesClass()
+yes.respondsToSelector(#selector(yes.sayYes))
 
-// 별도의 셀렉터 객체로
-let selector1 = Selector("sayYes")
-obj.respondsToSelector(selector1)
+// 같은 클래스의 다른 객체에 검사하기
+let yes2 = YesClass()
+yes2.respondsToSelector(#selector(yes.sayYes))
 
-// Selector 구조체 생성을 생략하고 메소드 식별 문자열로 사용 가능
-obj.respondsToSelector("sayNo")
-
-
-class MyClass2 : NSObject {
-   // NSObject의 init 메소드 재정의
-   override init() {
-   }
-   
-   init(who : String) {
-   }
-   
-   init(who : String, emotion : String) {
+class NoClass : NSObject {
+   func sayNo() {
+      print("NO!")
    }
 }
 
-
-var obj2 = MyClass2()
-
-obj2.respondsToSelector("init:") // false
-obj2.respondsToSelector("initWithWho:")
-obj2.respondsToSelector("initWithWho:emotion:")
-
-*/
+let no = NoClass()
+no.respondsToSelector(#selector(yes.sayYes))
