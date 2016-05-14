b8e3f9945a23959475e4862646acec5016bf8dd9
wannabewize <wannabewize+macbookpro@gmail.com>
2016-05-14 15:17:51 +0900

    C 스타일 대신 사용하는 for 문

--- 02.Controls/for.swift
+++ 02.Controls/for.swift
@@ -1,14 +1,38 @@
 // For Loop
-for var i = 0 ; i < 10 ; i++ {
-   // print(i);   
+let limit = 3
+
+// warning: C-style for statement is deprecated and will be removed in a future version of Swift
+for var i = 0 ; i < limit ; i++ {
+   print(i);   
 }
 
-var array = [1, 2, 3, 4, 5]
+// C 스타일 대신 사용
+for i in 0..<limit {
+   print(i)
+}
+
+// 배열 순회
+let array = [1, 2, 3]
 
 for item in array {
    print(item)
 }
 
+
+var array2 = [10, 20, 30]
+for item in array2 {
+   print(item)
+   
+   // 배열 순회 중 변경 시도. 과연? 
+   if item < 30 {
+      array2.append(item*2)
+   }
+}
+// 배열 순회 중 변경한 내용 - 순회 끝나고 출력
+print("Array2 : \(array2)")
+
+
+// 딕셔너리 순회
 var dic = ["일":"one", "이":"two", "삼":"three"]
 
 for (kor, eng) in dic {
@@ -16,6 +40,7 @@ for (kor, eng) in dic {
 }
 
 
+// 문자열 - 문자 순회
 var str = "Hello Swift"
 
 for ch in str.characters {
