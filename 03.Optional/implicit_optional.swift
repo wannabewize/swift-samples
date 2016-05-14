32a313f1da20edde5f7ff2cfec4a6bf83fc2ca37
wannabewize <wannabewize+macbookpro@gmail.com>
2016-05-14 15:18:49 +0900

    에러 메세지 추가

--- 03.Optional/implicit_optional.swift
+++ 03.Optional/implicit_optional.swift
@@ -1,7 +1,8 @@
 var optionalInt : Int! = 123
 
 let intVal = optionalInt.successor()
-intVal // 124, 옵셔널 타입이 아니다.
+print(intVal) // 124, 옵셔널 타입이 아니다.
 
 optionalInt = nil
-//let intVal2 = optionalInt.successor()
+// fatal error: unexpectedly found nil while unwrapping an Optional value
+// let intVal2 = optionalInt.successor()
