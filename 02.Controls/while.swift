843e4c97d0e8e3d3fb236283b1ab5c59eb180717
wannabewize <wannabewize+macbookpro@gmail.com>
2016-05-14 15:18:36 +0900

    ++ 표현 삭제 - deprecated

--- 02.Controls/while.swift
+++ 02.Controls/while.swift
@@ -1,14 +1,17 @@
+let count = 3
+
 var i = 0
-while i < 10 {
-   print("i = \(i++)")
+while i < count {
+   print("i = \(i)")
+   i += 1 // i++ 은 deprecated
 }
 
-
-
+// Do-While 대신
 var j = 0
 repeat {
-   print("j = \(j++)")
-   if j > 10 {
+   print("j = \(j)")
+   j += 1 // j++ 은 deprecated
+   if j > count {
       break			// break 로 반복 작업 끝내기
    }
 } while true
