8f80a78d018104efe0386390f5ed8ceab4784f94
wannabewize <wannabewize+macbookpro@gmail.com>
2016-05-15 21:45:06 +0900

    튜플 간 비교

--- 01.DataAndVariables/tuple.swift
+++ 01.DataAndVariables/tuple.swift
@@ -5,12 +5,15 @@ one.2 = "하나"
 
 print(one)
 
-
 let two = (num : 2, eng : "two", kor : "둘")
 
 print(two.num)		// 2
 print(two.kor)		// "둘"
 
+
+print("(1,1) == (1,1) : ",(1, "1") == (1, "1"))
+print("(1, one) < (2, two) : ", (1, "one") < (2, "two"))
+print("(1, one) < (1, first)", (1, "one") < (1, "first"))
 
 var second = two
 second.eng = "Second"
