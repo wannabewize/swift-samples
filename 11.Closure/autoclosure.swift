f94221f2ceea21b3cc5812ba1f22623787d62e66
wannabewize <wannabewize@gmail.com>
2016-05-16 02:06:04 +0900

    클로저 예제 : noescape, autoclosure

--- 11.Closure/autoclosure.swift
+++ 11.Closure/autoclosure.swift
@@ -1,17 +1,17 @@
-var value = 10
-
+var value = 0
 let closureExpress = { value += 10 }
+
+print(value) // 0
+closureExpress()
+print(value) // 10
 
 
 func doIt( arg : () -> () ) {
    arg()
-   print("normal example : ", value)
+   print(value)
 }
 
-
 doIt( {value += 10} )
-
-
 
 func doIt2(@autoclosure arg : () -> () ) {
    arg()
@@ -19,3 +19,18 @@ func doIt2(@autoclosure arg : () -> () ) {
 }
 
 doIt2(value += 10)
+
+
+/*
+ * autoclosure + escaping
+ */
+
+
+var escapeVar : (()->String)!
+
+func doIt3(@autoclosure(escaping) arg : () -> String ) {
+   print(arg())
+   escapeVar = arg
+}
+
+doIt3("value = \(value)")
--- 11.Closure/noescape.swift
+++ 11.Closure/noescape.swift
@@ -1,28 +1,64 @@
+var hiddenClosure : (() -> ())!
 func otherFunc( arg :  ()->() ) { }
 
+func doIt( arg : ()->() ) {
+   arg()
+   hiddenClosure = arg
+   otherFunc(arg)
+}
+
+var value = 0
+doIt ({
+   value += 10
+})
+print(value)
+
+
+hiddenClosure()
+
+
+func doIt2(@noescape arg : () -> () ) {
+   arg()
+   //   hiddenClosure = arg
+   //   otherFunc(arg)
+}
+
+// 두 개의 noescape 파라미터
+func doIt3(@noescape arg1 : ()->(), @noescape arg2 : () -> () ) {
+//   hiddenClosure = arg1
+//   hiddenClosure arg2
+   arg1()
+   arg2()
+}
+
+
+
+var otherProperty : (()->())!
+func escapeFunc(arg : ()->()) {
+   // 객체 소유 가능
+   otherProperty = arg
+   arg()
+}
+
+func noescapeFunc(@noescape arg : ()->() ) {
+   arg()
+}
+
 class MyClass {
-var closureProperty : (()->())!
-
-   func doIt(arg : ()->()) {
-      closureProperty = arg
-      otherFunc(arg)
-      arg()
+   var value = 0
+   
+   func doIt() {
+      escapeFunc({ self.value = 100 })
+      
+      noescapeFunc({ value = 200 })
    }
-
-   func doIt2() {
-      closureProperty()
-   }
-}
-
-func otherFunc2(@noescape arg : ()->()) {}
-
-class MyClass2 {
-var closureProperty : (()->())!
-
-   func doIt(@noescape arg : ()->()) {
-   //      closureProperty = arg
-   //      otherFunc(arg)
-      otherFunc2(arg)
-      arg()
+   
+   deinit {
+      print("MyClass 객체 해제")
    }
 }
+
+var obj : MyClass! = MyClass()
+obj.doIt()
+
+obj = nil
