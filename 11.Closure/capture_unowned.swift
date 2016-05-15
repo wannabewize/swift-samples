d9b6f4e0a4c607c7d9c20e890400ff15ffaae860
wannabewize <wannabewize+macbookpro@gmail.com>
2016-05-15 22:16:33 +0900

    클로저 캡춰 예제 정리

--- 11.Closure/capture_unowned.swift
+++ 11.Closure/capture_unowned.swift
@@ -1,30 +1,64 @@
-class MyClass {
-   var value = 0
+class Counter {
+   var value : Int = 0
    
-   func increseFn() -> ()->() {
+   func increment() -> () -> Int {
       return {
-        [unowned self] in
-         // [weak self] in
-         print(self?.value++)
+         self.value += 1
+         return self.value
       }
    }
    
    deinit {
-      print("MyClass 객체 해제")
+      print("Deinit")
    }
 }
 
-var obj : MyClass! = MyClass()
-var closure = obj.increseFn()
-closure()
+var obj : Counter! = Counter()
+
+var closure : ()->Int = obj.increment()
+print(closure())
+print(closure())
 
 obj = nil
 
-closure()
+print("== assign nil to closure")
+
+var obj2  : Counter! = Counter()
+var closure2 : (()->Int)! = obj2.increment()
+
+print(closure2())
+
+obj2 = nil
+closure2 = nil
 
 
 
+print("== Unonwing Self in Closure")
+//
+//
+//
+class Counter2 {
+   var value : Int = 0
+   
+   func increment() -> () -> Int {
+      return {
+         [unowned self] in
+         self.value += 1
+         return self.value
+      }
+   }
+   
+   deinit {
+      print("Deinit")
+   }
+}
 
+var obj3 : Counter2! = Counter2()
 
+var closure3 : (()->Int)! = obj3.increment()
+print(closure3())
+print(closure3())
 
+obj3 = nil
 
+// closure3()
