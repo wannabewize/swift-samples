29bed3e35275ebc2e77dd2564ab702268d92e6dc
wannabewize <wannabewize@gmail.com>
2016-10-20 01:16:26 +0900

    클로저 반환 예제 - 함수 내 함수로 변경

--- 11.Closure/closure_api.swift
+++ 11.Closure/closure_api.swift
@@ -6,13 +6,13 @@
 // 클로저를 반환하는 함수
 //
 
-// 함수 타입 : () -> ()
-func sayGoodmorning() {
-   print("Good Morning")
-}
-
 // ()->() 를 반환값의 타입으로 정의한 함수
 func greeting() -> () -> Void {
+   // 함수 타입 : () -> ()
+   func sayGoodmorning() {
+      print("Good Morning")
+   }
+
    return sayGoodmorning
 }
 
